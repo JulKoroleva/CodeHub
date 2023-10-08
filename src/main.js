7a5bbc38d6e4e2db03c7c316f9c97a493c7863c6
Julia Koroleva <jkorolevadev@gmail.com>
2023-10-08 19:27:36 +0300

    main.js edited

--- src/main.js
+++ src/main.js
@@ -1,19 +1,14 @@
-import Vue from 'vue';
+import { createApp } from 'vue';
 import App from './App.vue';
 import store from './store';
 import { InstallCodemirro } from "codemirror-editor-vue3";
-import router from './router';
+import router from './router'
 import Particles from "vue3-particles";
 
-new Vue({
-    router,
-    store,
-    render: h => h(App),
-    mounted() {
-        this.$nextTick(() => {
-            this.$use(InstallCodemirro, { componentName: "customName" });
-            this.$use(Particles);
-            this.$mount('#app');
-        });
-    }
-});
+
+const app = createApp(App).use(Particles);
+app.use(store);
+app.use(InstallCodemirro, { componentName: "customName" });
+app.use(Particles)
+app.use(router)
+app.mount('#app');
